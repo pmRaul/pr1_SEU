6cb7b07b8d262dadde33e2136dc7acbe295fc72b
Raúl Paredes <rpm00061@red.ujaen.es>
2023-03-24 09:20:23 +0100

    Proyecto finalizado

--- app.py
+++ app.py
@@ -4,37 +4,59 @@ import plotly.express as px
 from firebase_admin import credentials, firestore, initialize_app
 from datetime import datetime
 from streamlit_folium import folium_static
+import json
 
 # Inicializar la conexión con Firestore
 cred = credentials.Certificate('./baseDatosCredenciales.json')
 initialize_app(cred)
 db = firestore.client()
 
-# Obtener los datos de las caminatas
-caminatas_ref = db.collection('TodoData')
-caminatas = caminatas_ref.get()
+# Define a custom JSON encoder to serialize Timestamp and datetime objects
+class CustomJSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime):
+            return obj.isoformat()
+        return super().default(obj)
 
-datos = []
+# Función para obtener los datos de Firestore y guardarlos en un archivo local
+@st.cache(allow_output_mutation=True)
+def obtener_datos():
+    # Obtener los datos de las caminatas
+    caminatas_ref = db.collection('TodoData')
+    caminatas = caminatas_ref.get()
 
-# Añadir el campo de identificador único a cada ubicación
-for caminata in caminatas:
-    caminata_ = caminata.to_dict()
-    caminata_['datos'] = []
-    datos.append(caminata_)
+    datos = []
 
-# Obtener los datos de las ubicaciones
-ubicaciones_ref = db.collection('LocalizacionData')
-ubicaciones = ubicaciones_ref.get()
+    for caminata in caminatas:
+        caminata_ = caminata.to_dict()
+        caminata_['datos'] = []
+        datos.append(caminata_)
 
-# Añadir el campo de identificador único a cada ubicación
-for ubicacion in ubicaciones:
-    ubicacion_temp = ubicacion.to_dict()
-    for dato in datos:
-        if dato['id'] == ubicacion_temp['id']:
-            dato['datos'].append(ubicacion_temp)
+    # Obtener los datos de las ubicaciones
+    ubicaciones_ref = db.collection('LocalizacionData')
+    ubicaciones = ubicaciones_ref.get()
+
+    for ubicacion in ubicaciones:
+        ubicacion_temp = ubicacion.to_dict()
+        for dato in datos:
+            if dato['id'] == ubicacion_temp['id']:
+                dato['datos'].append(ubicacion_temp)
+
+    # Guardar los datos en un archivo local
+    with open('datos.json', 'w') as file:
+        json.dump(datos, file, cls=CustomJSONEncoder)
+
+    return datos
+
+# Obtener los datos de Firestore y guardarlos en un archivo local
+datos = obtener_datos()
 
 # Mostrar los datos en Streamlit
 st.title('Datos de las caminatas')
+
+# Crear un botón para actualizar los datos
+if st.button('Actualizar datos'):
+    datos = obtener_datos()
 
 # Crear un desplegable para seleccionar cada caminata
 caminata_seleccionada = st.selectbox('Selecciona una caminata:', [dato["id"] for dato in datos])
